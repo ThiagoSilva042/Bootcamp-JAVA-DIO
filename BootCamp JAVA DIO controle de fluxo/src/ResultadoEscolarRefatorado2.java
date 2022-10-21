932dcb21471db99e75d824e8e72e8f76586aec47
Thiago Silva <113719342+ThiagoSilva042@users.noreply.github.com>
2022-10-21 17:22:54 -0300

    Finalizando a aula de controle de fluxo
    Terminando alguns códigos finais da aula.

---
+++ BootCamp JAVA DIO controle de fluxo/src/ResultadoEscolarRefatorado2.java
@@ -0,0 +1,10 @@
+
+public class ResultadoEscolarRefatorado2 {
+	public static void main(String[] args) {
+		int nota = 7;
+		
+		String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		
+		System.out.println(resultado);
+	}
+}
