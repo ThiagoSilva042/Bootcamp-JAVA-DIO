703085ab93a29a7f8071ca55456cc030a1ae9f20
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2022-11-09 17:12:02 -0300

    Criando pasta e classe para estudo de OO

---
+++ BootCamp JAVA DIO Orientada a Objetos/src/Carro.java
@@ -0,0 +1,44 @@
+
+public class Carro {
+
+	String cor;
+	String modelo;
+	int capacidadeTanque;
+	
+	//Construtor
+	public Carro(String cor, String modelo, int capacidadeTanque) {
+		this.cor = cor;
+		this.modelo = modelo;
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+	//Gets e Sets
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getCapacidadeTanque() {
+		return capacidadeTanque;
+	}
+
+	public void setCapacidadeTanque(int capacidadeTanque) {
+		this.capacidadeTanque = capacidadeTanque;
+	}
+	
+	//Metodos
+	double totalValorTanque(double valorCombustivel) {
+		return capacidadeTanque * valorCombustivel;
+	}
+}
---
+++ BootCamp JAVA DIO collections/src/Desafios/Desafio.java
@@ -0,0 +1,24 @@
+package Desafios;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Desafio {
+	// Abaixo segue um exemplo de código que você pode ou não utilizar:
+
+	    public static void main(String[] args){
+
+	// TODO: Crie um Array de números inteiros:
+	     int[] num = {2, 3, 5, 7, 11, 13, 18, 34};
+
+	// TODO: Implemente um laço de repetição que verifique APENAS seus números Pares:
+	       
+	     for (int i = 0; i < num.length; i++){
+	         if (num[i] % 2 == 0){
+	                 System.out.println(num[i]);
+	                } 
+	            }
+	           
+    }
+
+}
+
